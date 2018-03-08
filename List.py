1a214e1d785878b8b9325b53bf6d8ba031392866
Eli Hermann <Storminmormon@hotmail.com>
2018-03-07 18:58:44 -0700

    Working

--- List.py
+++ List.py
@@ -1,4 +1,4 @@
-
+'''
 source = open(r"/users/elihermann/documents/github/food/source.txt",'w')
 
 source.write("Eli\n")
@@ -7,11 +7,23 @@ source.write("schools\n")
 source.write("fools")
 
 source.close()
-
+'''
 def grabLine(line):
     source = open(r"/users/elihermann/documents/github/food/source.txt",'r')
     for x in range (0,line):
         data = source.readline()
+    source.close()
     return data
 
-print(grabLine(2))
+def placeDish(content):
+    source = open(r"/users/elihermann/documents/github/food/source.txt",'a')
+    source.write("\nD "+content)
+    source.close()
+    
+def placeIngredient(content):
+    source = open(r"/users/elihermann/documents/github/food/source.txt",'a')
+    source.write("\nI "+content)
+    source.close()
+    
+    
+
