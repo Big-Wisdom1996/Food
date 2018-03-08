c994e54b222c2a3d6ea27a1fb544fcbd66db228e
Eli Hermann <Storminmormon@hotmail.com>
2018-03-07 20:34:14 -0700

    cooler

--- List.py
+++ List.py
@@ -15,6 +15,22 @@ def grabLine(line):
     source.close()
     return data
 
+def findInventory(ingredient):
+    done = 0
+    x = 1
+    while(done == 0):
+        #grab the type of line
+        type = grabLine(x).split(" ")
+        #compare type to desired type
+        if(type[0] == ingredient):
+            print("Yes")
+            done = 1
+        else:
+            x += 1 
+        
+    #return line
+    return type[1]
+    
 def placeDish(content):
     source = open(r"/users/elihermann/documents/github/food/source.txt",'a')
     source.write("\nD "+content)
@@ -25,5 +41,26 @@ def placeIngredient(content):
     source.write("\nI "+content)
     source.close()
     
+def placeInventory(content):
+    source = open(r"/users/elihermann/documents/github/food/source.txt",'a')
+    source.write("\nWInv "+content)
+    source.close()
     
-
+def placeActual(content):
+    source = open(r"/users/elihermann/documents/github/food/source.txt",'a')
+    source.write("\nAInv "+content)
+    source.close()
+    
+def newDish():
+    placeDish(input("What's the dish?: "))
+    done = 0
+    while(done == 0):
+        answer = input("Ingredient: ")
+        if (answer == "no"):
+            done = 1
+        else:
+            placeIngredient(answer)
+            placeActual("")
+            placeInventory(input("Weekly Inventory: "))
+            
+newDish()
