41a20af883e3048e26aba2766282f31e7db06762
Eli Hermann <Storminmormon@hotmail.com>
2018-03-09 15:13:21 -0700

    Pickle Testing

--- List.py
+++ List.py
@@ -1,15 +1,32 @@
 import pickle
-
-eli = 5
-thomas = 6
-
+    
+    
+'''
+#Serialize data
 outfile = open("/users/elihermann/documents/github/food/pickle.dat","wb")
 pickle.dump(eli, outfile)
 pickle.dump(thomas, outfile)
 outfile.close()
 
+#deserialize data
 infile = open("pickle.dat","rb")
 print(pickle.load(infile))
 print(pickle.load(infile))
 infile.close()
-            
+'''          
+
+def newDish(dishName):
+    outfile = open("/users/elihermann/documents/github/food/pickle.dat","wb")
+    pickle.dump(dishName,outfile)
+    outfile.close()
+    
+def readFile():
+    infile = open("/users/elihermann/documents/github/food/pickle.dat","rb")
+    print(pickle.load(infile))
+    infile.close()
+    
+newDish("Eli")
+newDish("Soup")
+readFile()
+readFile()
+
